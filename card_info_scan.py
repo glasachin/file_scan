19276cfee32827dedcc5bcd9f22143585b7eaa46
sachin <glasachin@yahoo.co.in>
2022-10-14 18:05:28 +0530

    odt_files

--- card_info_scan.py
+++ card_info_scan.py
@@ -6,23 +6,10 @@ from odf.opendocument import load
 import re
 import luhn_algo
 
-temp_files = ['txt_email_file.txt', 'txt_pass_file.txt', 'txt_card_file.txt']
+temp_files = ['txt_email_file.txt', 'txt_pass_file.txt', 'txt_card_file.txt', 'txt_passport_file.txt']
 for i in temp_files:
     f = open(i,'w')
     f.close
-
-# txt_email_files_path = []
-# txt_email_file = 'txt_email_file.txt'
-# f = open(txt_email_file,'w')
-# f.close()
-# txt_pass_file = 'txt_pass_file.txt'
-# f = open(txt_pass_file,'w')
-# f.close()
-
-# txt_card_file = 'txt_card_file.txt'
-# f = open(txt_card_file,'w')
-# f.close()
-
 
 if __name__ == "__main__":
     for (root,dirs,files) in os.walk('/home/sachin', topdown=True):
@@ -36,7 +23,22 @@ if __name__ == "__main__":
                         file_data = open(root+'/'+file,'r').read()
                         for txt_data in file_data.split('\n'):
                             txt_data_lower = txt_data.lower()
-                            
+                            # ----for email information-------
+                            txt_file_name = temp_files[0]
+                            regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+                            email_list = [s for s in re.findall(regex, txt_data_lower)]
+                            if len(email_list) > 0:
+                                if file in temp_files: # to skip cards from our own list
+                                    # print('own file')
+                                    continue
+                                email_file = open(txt_file_name, 'a')
+                                for i in email_list:
+                                    # if int(i) == 0: #to remove 00...00 entries
+                                    #     continue
+                                    i.replace(' ','')
+                                    email_file.write(i+'\n')
+                                email_file.close()
+
                             # ----For Cards Information-------
                             txt_file_name = temp_files[2]
                             # cards_list = [s for s in re.findall(r"\d{16}", txt_data_lower)]
@@ -55,14 +57,25 @@ if __name__ == "__main__":
                                         # print(root,file, i)
                                 card_file.close()
 
-                            # if ('email' in txt_data_lower)|('e-mail' in txt_data_lower):
-                            #     txt_email_files_path.append(root+'/'+file)
-                            #     open(txt_email_file, 'a').write(txt_data+'\n')
-                            # if ('password' in txt_data_lower)|('pass' in txt_data_lower):
-                            #     txt_email_files_path.append(root+'/'+file)
-                            #     open(txt_pass_file, 'a').write(txt_data+'\n')
+                            # ----For Passport Information-------
+                            txt_file_name = temp_files[3]
+                            regex = "^[A-PR-WYa-pr-wy][1-9]\\d\\s?\\d{4}[1-9]$"
+                            passport_list = [s for s in re.findall(regex, txt_data_lower)]
+                            if len(passport_list) > 0:
+                                if file in temp_files: # to skip cards from our own list
+                                    # print('own file')
+                                    continue
+                                passport_file = open(txt_file_name, 'a')
+                                for i in passport_list:
+                                    # if int(i) == 0: #to remove 00...00 entries
+                                    #     continue
+                                    i.replace(' ','')
+                                    passport_file.write(i+'\n')
+                                passport_file.close()
 
-                    # elif ('.odt' in file):
+
+
+                            # elif ('.odt' in file):
                     #     textdoc = load(root+'/'+file)
                     #     allparas = textdoc.getElementsByType(text.P)
                     #     for odt_par in allparas:
